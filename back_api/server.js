e7e5a31a7f94e2921c20da1b069eafe999eeb348
pablo.aparici <pablo.aparici@estudiantat.upc.edu>
2024-03-15 21:29:50 +0100

    Implementació de la cache al server per guardar les activitats

--- back_api/server.js
+++ back_api/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 
+const NodeCache = require("node-cache");
+
 const admin = require("firebase-admin");
 const credentials = require("./key.json");
 
@@ -16,6 +18,41 @@ app.use(express.urlencoded({extended: true}));
 const db = admin.firestore();
 
 const PORT = process.env.PORT || 8080;
+
+// Crear una nueva caché con un TTL estándar y un período de revisión
+const myCache = new NodeCache({ stdTTL: 86400, checkperiod: 8700 });
+
+// Función para obtener datos de la API y actualizar Firestore
+async function updateDataCacheAndFireStore() {
+    try {
+        // Realizar la solicitud GET a la API utilizando axios
+        const response = await axios.get('https://analisi.transparenciacatalunya.cat/resource/rhpv-yr4f.json');
+        // Verificar si la respuesta fue exitosa (código de respuesta 200)
+        if (response.status === 200) {
+            // Obtener los datos JSON de la respuesta
+            const data = response.data;
+            // Actualizar la caché con los nuevos datos
+            myCache.set("actividades", data);
+            
+            await updateFireStore(response.data);
+
+        } else {
+            console.error(`Error al obtener datos desde la API. Código de respuesta: ${response.status}`);
+        }
+    } catch (error) {
+        console.error('Error al obtener datos desde la API:', error.message);
+    }
+}
+
+async function updateFireStore() {
+    //logica actualizar FireStore con el JSON obtenido.
+}
+
+// Evento que se dispara cuando un elemento de la caché expira
+myCache.on("expired", function(key, value) {
+    console.log(`El elemento con la clave ${key} ha expirado, actualizando datos...`);
+    updateDataCacheAndFireStore();
+});
 
 app.post('/create', async (req, res) => {
     try{
@@ -44,8 +81,6 @@ app.get('/read/all', async (req, res) => {
         res.send(error);
     }
 });
-
-
 
 //Obtener 1 documento
 app.get('/read/:id', async (req, res) => {
@@ -77,6 +112,10 @@ app.get('/activitats/agenda/json', async (req, res) => {
         console.error('Error al obtener datos desde la API:', error.message);
         return null;
     }
+});
+
+app.get('/activitats/cache', async (req,  res) => {
+    res.status(200).send(myCache.get("actividades"));
 });
 
 app.get('/activitats/categoria', async (req, res) => {
@@ -135,6 +174,11 @@ app.get('/activitats/date', async (req, res) => {
     }
 });*/
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+    // Inicializar la caché con datos por primera vez
+    await updateDataCacheAndFireStore();
     console.log(`Server is working on PORT ${PORT}`);
 });
+
+
+
