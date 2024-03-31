58120072f2d848e88e8afc60906de13d9bdc07dc
Eman0209 <rosa.hortelano@estudiantat.upc.edu>
2024-03-31 18:43:49 +0200

    actualitzacio back del foro

--- back_api/app.js
+++ back_api/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const activitatsRoutes = require('./routes/activitats')
 const usersRoutes = require('./routes/users')
+const forosRoutes = require('./routes/foros')
 const app = express()
 
 app.use('/activitats', activitatsRoutes)
 app.use('/users', usersRoutes)
+app.use('/foros', forosRoutes)
 
 module.exports = app
