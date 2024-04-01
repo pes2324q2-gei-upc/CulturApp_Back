59918be025ebc928c2eb05a8c040990ff09ce13e
josep.valero.casas-aljama <josep.valero.casas-aljama@estudiantat.upc.edu>
2024-04-01 13:59:19 +0200

    Llegir body arreglat

--- back_api/routes/activitats.js
+++ back_api/routes/activitats.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+router.use(express.json());
+
 const { db } = require('../firebaseConfig');
 
 router.get('/read/all', async (req, res) => {
