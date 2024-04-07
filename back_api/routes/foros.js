75927c50cb4d6f700e5607efe6e4400af7b07898
Eman0209 <rosa.hortelano@estudiantat.upc.edu>
2024-04-07 11:03:04 +0200

    get + posts de replies a un post del foro

--- back_api/routes/foros.js
+++ back_api/routes/foros.js
@@ -84,7 +84,6 @@ router.get('/:foroId/posts', async (req, res) => {
     }
 });
 
-
 //publica un post
 // Ruta para agregar un nuevo post a un foro existente
 router.post('/:foroId/posts', async (req, res) => {
@@ -124,6 +123,75 @@ router.put('/:foroId/posts/:postId', async(req, res) => {
 
 //elimina un post
 router.delete('/:foroId/posts/:postId', async(req, res) => {
+});
+
+//crea una reply
+router.post('/:foroId/posts/:postId/reply', async (req, res) => {
+    console.log("intenta crear un post")
+    try {
+        const { username, mensaje, fecha, numero_likes } = req.body;
+        const foroId = req.params.foroId;
+        const postId = req.params.postId;
+
+        // Verificar si el foro existe
+        const foroRef = db.collection('foros').doc(foroId);
+        const foroSnapshot = await foroRef.get();
+
+        if (!foroSnapshot.exists) {
+            res.status(404).send("Foro no encontrado");
+            return;
+        }
+
+        // Verificar si el post existe
+        const postRef = foroRef.collection('posts').doc(postId);
+        const postSnapshot = await postRef.get();
+
+        if (!postSnapshot.exists) {
+            res.status(404).send("Post no encontrado");
+            return;
+        }
+
+        // Agregar el nuevo post al foro
+        await postRef.collection('reply').add({
+            username: username,
+            mensaje: mensaje,
+            fecha: fecha,
+            numero_likes: numero_likes
+        });
+
+        res.status(201).send("Reply agregada exitosamente al foro");
+    } catch (error) {
+        console.error("Error al agregar reply al post:", error);
+        res.status(500).send("Error interno del servidor");
+    }
+});
+
+//get replies
+router.get('/:foroId/posts/:postId/reply', async (req, res) => {
+    try {
+        const foroId = req.params.foroId;
+        const postId = req.params.postId;
+        
+        // Obtener los posts del foro con el foroId especificado
+        const postsRef = db.collection('foros').doc(foroId).collection('posts').doc(postId).collection('reply');
+        const snapshot = await postsRef.get();
+
+        if (snapshot.empty) {
+            console.log('No hay replies encontrados para el post con el ID:', foroId);
+            res.status(404).send('No hay replies encontrados para el post');
+            return;
+        }
+
+        let posts = [];
+        snapshot.forEach(doc => {
+            posts.push(doc.data());
+        });
+
+        res.status(200).json(posts);
+    } catch (error) {
+        console.error('Error al obtener las replies del foro:', error);
+        res.status(500).send('Error interno del servidor');
+    }
 });
 
 module.exports = router
