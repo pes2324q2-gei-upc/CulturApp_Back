2ab774faeca9412aca52beda7517fa75a56c01d3
josep.valero.casas-aljama <josep.valero.casas-aljama@estudiantat.upc.edu>
2024-04-14 10:33:00 +0200

    Collections reportsBug, reportsUsuaris i solicitudsOrganitzador i operacions basiques

--- back_api/app.js
+++ back_api/app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const activitatsRoutes = require('./routes/activitats')
 const usersRoutes = require('./routes/users')
 const amicsRoutes = require('./routes/amics')
+const ticketsRoutes = require('./routes/tickets')
 const app = express()
 
 app.use('/activitats', activitatsRoutes)
 app.use('/users', usersRoutes)
 app.use('/amics', amicsRoutes)
+app.use('/tickets', ticketsRoutes)
 
 module.exports = app
---
+++ back_api/routes/tickets.js
@@ -0,0 +1,279 @@
+const admin = require('firebase-admin')
+const express = require('express')
+const router = express.Router()
+
+router.use(express.json());
+
+const { db } = require('../firebaseConfig');
+
+//Operacions de reports d'usuari
+router.post('/create/reportUsuari', async(req, res) => {
+    try {
+        const { uid, report, usuariReportat } = req.body;
+
+        const reportsCollection = db.collection('reportsUsuaris');
+
+        await reportsCollection.add({
+            'user': uid,
+            'motiuReport': report,
+            'usuariReportat': usuariReportat,
+            'solucionat': false
+        })
+        res.status(200).send('OK')
+    }
+    catch (error){
+        res.send(error);
+    }
+});
+
+router.get('/read/reportsUsuari/all', async (req, res) => {
+    try {
+        const reportsRef = db.collection('reportsUsuaris');
+        const response = await reportsRef.get();
+        let responseArr = [];
+        response.forEach(doc => {
+            responseArr.push(doc.data());
+        });
+        res.status(200).send(responseArr);
+    } catch (error){
+        res.send(error);
+    }
+});
+
+router.get('/read/repostsUsuaris/pendents', async(req, res) => {
+    try {
+        const reportsRef = db.collection('reportsUsuaris').where('solucionat', '==', false);
+        const response = await reportsRef.get();
+        let responseArr = [];
+        response.forEach(doc => {
+            responseArr.push(doc.data());
+        });
+        res.status(200).send(responseArr);
+    } 
+    catch (error){
+        res.send(error);
+    }
+});
+
+router.get('/read/repostsUsuaris/solucionats', async(req, res) => {
+    try {
+        const reportsRef = db.collection('reportsUsuaris').where('solucionat', '==', true)
+        const response = await reportsRef.get();
+        let responseArr = [];
+        response.forEach(doc => {
+            responseArr.push(doc.data());
+        });
+        res.status(200).send(responseArr);
+    }
+    catch (error){
+        res.send(error);
+    }
+});
+
+router.put('/solucionat/reportUsuari/:id', async(req, res) => {
+    try {
+        const id  = req.params.id;
+        const reportRef = db.collection('reportsUsuaris').doc(id);
+        await reportRef.update({
+            'solucionat': true
+        });
+        res.status(200).send('OK');
+    } catch (error){
+        res.send(error);
+    }
+});
+
+router.delete('/delete/reportUsuari', async(req, res) => {
+    try {
+        const { id } = req.body;
+        const reportRef = db.collection('reportsUsuaris').doc(id);
+        await reportRef.delete();
+        res.status(200).send('OK');
+    } catch (error){
+        res.send(error);
+    }
+});
+
+//Operacions de reports de bugs
+router.post('/create/reportBug', async(req, res) => {
+    try {
+        const { uid, report } = req.body;
+
+        const reportsCollection = db.collection('reportsBugs');
+
+        await reportsCollection.add({
+            'user': uid,
+            'errorApp': report,
+            'solucionat': false
+        })
+        res.status(200).send('OK')
+    }
+    catch (error){
+        res.send(error);
+    }
+});
+
+router.get('/read/reportsBug/all', async (req, res) => {
+    try {
+        const reportsRef = db.collection('reportsBugs');
+        const response = await reportsRef.get();
+        let responseArr = [];
+        response.forEach(doc => {
+            responseArr.push(doc.data());
+        });
+        res.status(200).send(responseArr);
+    } catch (error){
+        res.send(error);
+    }
+});
+
+router.get('/read/reportsBugs/pendents', async(req, res) => {
+    try {
+        const reportsRef = db.collection('reportsBugs').where('solucionat', '==', false);
+        const response = await reportsRef.get();
+        let responseArr = [];
+        response.forEach(doc => {
+            responseArr.push(doc.data());
+        });
+        res.status(200).send(responseArr);
+    } 
+    catch (error){
+        res.send(error);
+    }
+});
+
+router.get('/read/reportsBugs/solucionats', async(req, res) => {
+    try {
+        const reportsRef = db.collection('reportsBugs').where('solucionat', '==', true);
+        const response = await reportsRef.get();
+        let responseArr = [];
+        response.forEach(doc => {
+            responseArr.push(doc.data());
+        });
+        res.status(200).send(responseArr);
+    } 
+    catch (error){
+        res.send(error);
+    }
+});
+
+router.put('/solucionat/reportBug/:id', async(req, res) =>{
+    try {
+        const id  = req.params.id;
+        const reportRef = db.collection('reportsBugs').doc(id);
+        await reportRef.update({
+            'solucionat':true
+        })
+        res.status(200).send('OK')
+    }
+    catch(error) {
+        res.send(error)
+    }
+});
+
+router.delete('/delete/reportBug', async(req, res) => {
+    try {
+        const { id } = req.body;
+        const reportRef = db.collection('reportsBugs').doc(id);
+        await reportRef.delete();
+        res.status(200).send('OK');
+    } catch (error){
+        res.send(error);
+    }
+});
+
+//Operacions de sol·licituds d'organitzador
+router.post('/create/solicitudOrganitzador', async(req, res) => {
+    try {
+        const { uid, email, idActivitat } = req.body;
+        const solictudRef = db.collection('solicitudsOrganitzador');
+        const activitatRef = db.collection('actividades').doc(idActivitat);
+        const doc = await activitatRef.get()
+        const dataFi = doc.data().data_fi;
+        await solictudRef.add({
+            'userSolicitant': uid,
+            'email': email,
+            'idActivitat': idActivitat,
+            'dataFi': dataFi,
+            'atorgat': false
+        })
+        res.status(200).send('OK');
+    }
+    catch (error){
+        res.send(error)
+    }
+});
+
+router.get('/read/solicitudsOrganitzador/pendents', async(req, res) => {
+    try {
+        const solicitudsRef = db.collection('solicitudsOrganitzador').where('otorgat', '==', false);
+        const response = await solicitudsRef.get();
+        let responsArr = [];
+        response.forEach(doc => {
+            responsArr.push(doc.data());
+        });
+        res.status(200).send(responsArr);
+    }
+    catch (error){
+        res.send(error);
+    }
+});
+
+router.get('/read/solicitudsOrganitzador/otorgades', async(req, res) => {
+    try {
+        const solicitudsRef = db.collection('solicitudsOrganitzador').where('otorgat', '==', true);
+        const response = await solicitudsRef.get();
+        let responsArr = [];
+        response.forEach(doc => {
+            responsArr.push(doc.data());
+        });
+        res.status(200).send(responsArr);
+    }
+    catch (error){
+        res.send(error);
+    }
+});
+
+router.get('/read/solicitudsOrganitzador/caducades', async(req, res) => {
+    try {
+        var date = new Date();
+        date = date.toISOString();
+        date = date.replace('Z', '');
+        const solicitudsRef = db.collection('solicitudsOrganitzador').where('dataFi', '<', date);
+        const response = await solicitudsRef.get();
+        let responsArr = [];
+        response.forEach(doc => {
+            responsArr.push(doc.data());
+        });
+        res.status(200).send(responsArr);
+    }
+    catch (error){
+        res.send(error);
+    }
+});
+
+router.put('/acceptar/solicitudOrganitzador/:id', async(req, res) => {
+    try {
+        const id = req.params.id;
+        const solicitudRef = db.collection('solicitudsOrganitzador').doc(id);
+        /*
+        Depenent de com volguem tenir les collections d'organitzadors, activitats i usauris es posarà com calgui
+
+        const doc = await solicitudRef.get();
+        const usauriOrganitzador = doc.data().userSolicitant;
+        const userRef = db.collection('users').doc(usauriOrganitzador);
+        await userRef.update({
+            'organitzador': true
+        });
+        */
+        await solicitudRef.update({
+            'atorgat': true
+        })
+        res.status(200).send('OK');
+    }
+    catch (error){
+        res.send(error);
+    }
+});
+
+module.exports = router;
