49d93cceb1d6a37deba461d133a22c8a8c92f688
Rosa <rosa.hortelano@estudiantat.upc.edu>
2024-04-18 13:53:46 +0200

    Post missatge al back

--- back_api/routes/grups.js
+++ back_api/routes/grups.js
@@ -18,13 +18,46 @@ router.post('/create', async(req, res) => {
             'descripcio': descr,
             'imatge': imatge,
             //'creador': admin,
-            'members': members
+            'members': members,
+            'last_msg': ' ',
+            'last_time': ' '
         });
 
         res.status(201).send({ message: "Grup creado exitosamente", id: docRef.id });
     }
     catch (error){
         console.error("Error al crear el grupo:", error);
+        res.status(500).send("Error interno del servidor");
+    }
+});
+
+//post de mensajes 
+router.post('/:grupId/mensajes', async (req, res) => {
+    try {
+        const { senderId, receiverId, mensaje, fecha } = req.body;
+        const grupId = req.params.grupId;
+
+        // Verificar si el xat existe
+        const grupRef = db.collection('grups').doc(grupId);
+        const grupSnapshot = await grupRef.get();
+
+        //si no existe crear?
+        if (!grupSnapshot.exists) {
+            res.status(404).send("Grup no encontrado");
+            return;
+        }
+
+        // Agregar el nuevo mensaje al xat
+        await grupRef.collection('mensajes').add({
+            senderId: senderId,
+            receiverId: receiverId,
+            mensaje: mensaje,
+            fecha: fecha
+        });
+
+        res.status(201).send("Mensaje agregado exitosamente al grup");
+    } catch (error) {
+        console.error("Error al agregar mensaje al grup:", error);
         res.status(500).send("Error interno del servidor");
     }
 });
--- back_api/routes/xats.js
+++ back_api/routes/xats.js
@@ -6,6 +6,7 @@ router.use(express.json());
 
 const { db } = require('../firebaseConfig');
 
+//crear xat
 router.post('/create', async(req, res) => {
     try {
         console.log("Solicitud recibida en la ruta '/xats/create'");
@@ -23,6 +24,38 @@ router.post('/create', async(req, res) => {
     }
     catch (error){
         console.error("Error al crear el xat:", error);
+        res.status(500).send("Error interno del servidor");
+    }
+});
+
+//post de mensajes 
+router.post('/:xatId/mensajes', async (req, res) => {
+    console.log("intenta crear un missatge")
+    try {
+        const { senderId, receiverId, mensaje, fecha } = req.body;
+        const xatId = req.params.xatId;
+
+        // Verificar si el xat existe
+        const xatRef = db.collection('xats').doc(xatId);
+        const xatSnapshot = await xatRef.get();
+
+        //si no existe crear?
+        if (!xatSnapshot.exists) {
+            res.status(404).send("Xat no encontrado");
+            return;
+        }
+
+        // Agregar el nuevo mensaje al xat
+        await xatRef.collection('mensajes').add({
+            senderId: senderId,
+            receiverId: receiverId,
+            mensaje: mensaje,
+            fecha: fecha
+        });
+
+        res.status(201).send("Mensaje agregado exitosamente al xat");
+    } catch (error) {
+        console.error("Error al agregar mensaje al xat:", error);
         res.status(500).send("Error interno del servidor");
     }
 });
