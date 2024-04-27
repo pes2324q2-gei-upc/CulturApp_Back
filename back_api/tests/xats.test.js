f10120b6a60e17bc63e102c39ccb89d1b8b93d66
Eman0209 <rosa.hortelano@estudiantat.upc.edu>
2024-04-27 10:57:57 +0200

    començar test

---
+++ back_api/tests/xats.test.js
@@ -0,0 +1,40 @@
+const request = require('supertest');
+const app = require('../app'); 
+
+//post crear xat
+
+//post mensajes
+
+//get mensajes 
+describe('GET /xats/:xatId/mensajes', () => {
+    it('should return all mesages of a xat', async () => {
+      // Add xats messages to the database
+      const xatsMessage = [
+        {
+            fecha: '2024-03-19T09:53:36.619810',
+            mensaje: 'Hola',
+            receiverId: '2aR4ekwm4RUYdvPgteZLdSJUew04',
+            senderId: 'VyRLuLpe7bNHvoP5DfuKQY1nrrw2', 
+        },
+        {
+            fecha: '2024-04-19T10:53:36.619810',
+            mensaje: 'Bona tarda',
+            receiverId: 'VyRLuLpe7bNHvoP5DfuKQY1nrrw2',
+            senderId: 'lQLMXlk8ykgpaaof8J0gplno20k1', 
+        },
+      ];
+  
+      for (const msg of xatsMessage) {
+        await db.collection('xats').add(msg);
+      }
+  
+      const res = await request(app).get('/xats/:xatId/mensajes');
+  
+      expect(res.statusCode).toEqual(200);
+      expect(res.body).toHaveLength(xatsMessage.length);
+  
+      for (const msg of xatsMessage) {
+        expect(res.body).toContainEqual(msg);
+      }
+    });
+  });
