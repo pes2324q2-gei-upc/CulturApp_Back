074278cf9748a5ca9b18e666b7e868d40304ec70
Rosa <140391097+Eman0209@users.noreply.github.com>
2024-05-02 12:59:38 +0200

    Revert "Merge amb xats/grups"

--- back_api/routes/activitats.js
+++ back_api/routes/activitats.js
@@ -6,11 +6,10 @@ router.use(express.json());
 
 const { db } = require('../firebaseConfig');
 
-const checkPerson = require('./middleware').checkPerson;
-
-router.get('/read/all', checkPerson, async (req, res) => {
+router.get('/read/all', async (req, res) => {
     try {
-        const activityRef = db.collection("actividades").limit(20);
+
+        const activityRef = db.collection("actividades").limit(100);
         const response = await activityRef.get();
         let responseArr = [];
         response.forEach(doc => {
@@ -18,11 +17,11 @@ router.get('/read/all', checkPerson, async (req, res) => {
         });
         res.status(200).send(responseArr);
     } catch (error){
-        res.send(404).send(error);
+        res.send(error);
     }
 });
 
-router.get('/categoria/:categoria', checkPerson, async (req, res) => {
+router.get('/categoria/:categoria', async (req, res) => {
     try {
         var cats = req.params.categoria.split(',');
         const activityRef = db.collection("actividades").where('tags_categor_es', 'array-contains-any', cats);
@@ -33,11 +32,11 @@ router.get('/categoria/:categoria', checkPerson, async (req, res) => {
         });
         res.status(200).send(responseArr);
     } catch (error){
-        res.send(404).send(error);
+        res.send(error);
     }
 });
 
-router.get('/date/:date', checkPerson, async (req, res) => {
+router.get('/date/:date', async (req, res) => {
     try {
         var date = req.params.date;
         date = date.slice(0, -4);
@@ -51,65 +50,33 @@ router.get('/date/:date', checkPerson, async (req, res) => {
         });
         res.status(200).send(responseArr);
     } catch (error){
-        res.send(404).send(error);
+        res.send(error);
     }
 });
 
-router.get('/name/:name', checkPerson, async (req, res) => {
+router.get('/name/:name', async (req, res) => {
     try {
         var nomAct = req.params.name;
         const activityRef = db.collection("actividades").where('denominaci', '==', nomAct);
         const response = await activityRef.get();
-
-        if(response.empty) return res.status(404).send('Actividad no encontrada');
-
         let responseArr = [];
         response.forEach(doc => {
             responseArr.push(doc.data());
         });
         res.status(200).send(responseArr);
     } catch (error){
-        res.send(404).send(error);
+        res.send(error);
     }
 });
 
-router.get('/read/:id', checkPerson, async (req, res) => {
+router.get('/read/:id', async (req, res) => {
     try {
-        const actId = req.params.id;
-        const activityRef = db.collection("actividades").doc(actId);
+        const activityRef = db.collection("actividades").doc(req.params.id);
         const response = await activityRef.get();
-        if(!response.exists) return res.status(404).send('Actividad no encontrada');
-        res.status(200).send(response.data());
+        res.send(response.data());
     } catch (error){
-        res.send(407).send(error);
+        res.send(error);
     }
 });
-
-router.get('/mediambient', checkPerson, async (req, res) => {
-
-    /*global.callCount++;
-    if (global.callCount > 0) {
-        return res.status(429).send('Numero de crides diaries superat');
-    }*/
-
-    try {
-        let date = new Date().toISOString();
-        date = date.replace('Z', '');
-        const activityRef = db.collection("actividades").where('tags_categor_es', 'array-contains-any', ['Residus', 'Sostenibilitat'])
-                                                        .where('data_inici', '>=', date)
-                                                        .orderBy('data_inici', 'asc');
-        const response = await activityRef.get();
-        let responseArr = [];
-        response.forEach(doc => {
-            responseArr.push(doc.data());
-        });
-        res.status(200).send(responseArr);
-    }
-    catch(error){
-        res.status(404).send(error);
-    }
-
-});
-
 
 module.exports = router
--- back_api/server.js
+++ back_api/server.js
@@ -1,8 +1,17 @@
 const app = require('./app');
+
+const NodeCache = require("node-cache");
 require('dotenv').config();
 
+const{db, auth} = require('./firebaseConfig');
+
 const PORT = process.env.PORT;
+
 
 app.listen(PORT, async () => {
     console.log(`Server is working on PORT ${PORT}`);
 });
+
+
+
+
