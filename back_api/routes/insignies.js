977ed677d7cb054c60a679d23473793dcae2e12c
josep.valero.casas-aljama <josep.valero.casas-aljama@estudiantat.upc.edu>
2024-05-24 17:20:55 +0200

    ordenar insignies

--- back_api/routes/insignies.js
+++ back_api/routes/insignies.js
@@ -30,10 +30,39 @@ router.get('/user/:username', checkUserAndFetchData, async (req, res) => {
             res.status(404).send('Insignies not found');
             return;
         }
-        res.status(200).send(insigniesDoc.data());
+        const insigniesData = insigniesDoc.data();
+        const keys = Object.keys(insigniesData);
+
+        keys.sort((a, b) => {
+            const aValue = insigniesData[a];
+            const bValue = insigniesData[b]
+            // Ordenar por la primera componente
+            const order = ['o', 'p', 'b', 'None'];
+            const aIndex = order.indexOf(aValue[0]);
+            const bIndex = order.indexOf(bValue[0]);
+        
+            if (aIndex !== bIndex) return aIndex - bIndex;
+
+            // Si la primera componente es igual, ordenar por la segunda componente
+            if (aValue[1] > bValue[1]) return -1;
+            if (aValue[1] < bValue[1]) return 1;
+
+            // Si la segunda componente también es igual, ordenar alfabéticamente
+            return a.localeCompare(b);
+        });
+
+        // Crear un nuevo objeto con las claves ordenadas
+        const sortedInsigniesData = {};
+        keys.forEach(key => {
+            sortedInsigniesData[key] = insigniesData[key];
+        });
+
+        res.status(200).send(sortedInsigniesData);
     } catch (error) {
         res.status(500).send(error);
     }
 });
+
+
 
 module.exports = router;
