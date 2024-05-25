eeaf016b14fd863615a143ba73cf49f73ed609de
susssss23 <susannajaner@gmail.com>
2024-05-25 22:12:35 +0200

    fet tests de notifiacions

---
+++ back_api/tests/notificacio.test.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+const app = require('../app'); 
+
+const crypto = require('crypto');
+const algorithm = 'aes-256-cbc';
+const key = Buffer.from(process.env.ENCRYPTION_KEY, 'hex');
+const iv = Buffer.from(process.env.ENCRYPTION_IV, 'hex');
+
+function encrypt(text) {
+  let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+}
+
+describe('POST /notificacio/enviar', () => {
+    it('should create a notification', async () => {
+        const res = await request(app)
+        .post('/notificacio/enviar')
+        .send({
+            title: 'titleTest',
+            mensaje: 'mensajeTest',
+            token: 'tokenTest'
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.text).toBe('OK');
+  
+    });
+    
+});
