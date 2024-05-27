484dddd2095eb5b86ffcba34532348f9eff7efa2
susssss23 <susannajaner@gmail.com>
2024-05-27 21:12:38 +0200

    trying

--- back_api/tests/notificacio.test.js
+++ back_api/tests/notificacio.test.js
@@ -13,56 +13,18 @@ function encrypt(text) {
   return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
 }
 
-const assert = require('assert');
-const sinon = require('sinon');
-const request = require('supertest');
-const server = require('../path/to/your/express/app'); // Adjust the path to your Express app
-const admin = require('firebase-admin'); // Assuming you are using Firebase Admin SDK
+describe('POST /notificacio/enviar', () => {
+    it('should create a notification', async () => {
+        const encryptedData = encrypt(JSON.stringify({
+            title: 'titleTest',
+            mensaje: 'mensajeTest',
+            token: 'tokenTest'
+        }));
 
-describe('POST /enviar', () => {
-    let sendStub;
-
-    beforeEach(() => {
-        sendStub = sinon.stub(admin.messaging(), 'send');
+        const res = await request(app)
+            .post('/notificacio/enviar')
+            .send(encryptedData); 
+  
     });
-
-    afterEach(() => {
-        sendStub.restore();
-    });
-
-    it('should send a notification and return status 201', (done) => {
-        sendStub.resolves('mockedResponse');
-
-        request(server)
-            .post('/enviar')
-            .send({
-                title: 'Test Title',
-                mensaje: 'Test Message',
-                token: 'mockedToken',
-            })
-            .expect(201)
-            .end((err, res) => {
-                if (err) return done(err);
-                assert.strictEqual(res.text, 'Notificacio enviada');
-                done();
-            });
-    });
-
-    it('should return status 500 if there is an error sending the notification', (done) => {
-        sendStub.rejects(new Error('mockedError'));
-
-        request(server)
-            .post('/enviar')
-            .send({
-                title: 'Test Title',
-                mensaje: 'Test Message',
-                token: 'mockedToken',
-            })
-            .expect(500)
-            .end((err, res) => {
-                if (err) return done(err);
-                assert.strictEqual(res.text, 'Error enviant la notificació');
-                done();
-            });
-    });
+    
 });
